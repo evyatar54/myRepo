20dd6e6ffb7e8bf1c67f70ca7c4d382541fdc380
evyatar54 <evyatar.sivan@gmail.com>
2015-05-17 16:22:33 +0300

    remove list after searching

--- statusBar.py
+++ statusBar.py
@@ -1,25 +1,23 @@
-
-
 import gtk
 
 class ProgressWin(gtk.Window):
     
     def __init__(self,n):
 	gtk.Window.__init__(self,type=gtk.WINDOW_TOPLEVEL)
-	
 	self.vbox = gtk.VBox()
 	self.query_num = n
 
 	self.status = gtk.Label()
-	self.status.set_size_request(800,40)
+	self.status.set_size_request(600,30)
 	self.vbox.pack_start(self.status)
 	
 	self.pb = gtk.ProgressBar()
-	self.pb.set_size_request(800,40)
+	self.pb.set_size_request(600,30)
 	self.vbox.pack_start(self.pb)
 	
 	self.add(self.vbox)
 	self.show_all()
+	self.set_keep_above(True)
 	
     def set_status(self,status):
 	self.status.set_text(status)
